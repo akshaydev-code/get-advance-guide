feed5ef1a11d9ca4bf644123c3e78994b958011e
Akshay0922 <akshayrealvlogs@gmail.com>
2026-05-15 21:52:00 +0530

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="h-screen flex items-center justify-center">
       <h1 className="text-5xl font-bold">
-        Get Advance Guide
+        Get Advance Guide home page
       </h1>
     </main>
   );
